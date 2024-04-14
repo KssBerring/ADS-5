e0fd36fa54defbfba9fda0f35733494861d645b2
KssBerring <160275047+KssBerring@users.noreply.github.com>
2024-04-14 18:15:42 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -5,6 +5,31 @@
 template<typename T, int size>
 class TStack {
   // добавьте код стека
+ private:
+    T data[100];
+    int top;
+
+ public:
+    TStack(): top(-1) {}
+    void push(T val) {
+      if (!isOverflow())
+        data[++top] = val;
+      else
+        throw std::string("Full");
+    }
+    T see() const { return data[top]; }
+    T pop() {
+      if (isEmpty())
+        throw std::string("Empty");
+      else
+        return data[top--];
+    }
+    bool isOverflow() const {
+      return top == size - 1;
+    }
+    bool isEmpty() const {
+      return top == -1;
+    }
 };
 
 #endif  // INCLUDE_TSTACK_H_
