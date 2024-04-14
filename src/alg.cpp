a2aa97dacfc28097294d40db7fa7ec73f35cbf56
KssBerring <160275047+KssBerring@users.noreply.github.com>
2024-04-14 17:36:03 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,68 +5,115 @@
 #include "alg.h"
 std::string infx2pstfx(std::string inf) {
   // добавьте код
-  TStack<char, 100> stack;
-    std::string postfix = "";
-
-    for (char c : inf) {
-        if (std::isdigit(c)) {
-            postfix += c;
-        } else if (c == '(') {
-            stack.push(c);
-        } else if (c == ')') {
-            while (!stack.isEmpty() && stack.top() != '(') {
-                postfix += " ";
-                postfix += stack.top();
-                stack.pop();
+  TStack<char, 100> stack1;
+    std::string end;
+    for (int i = 0; i < inf.length(); i++) {
+        if (inf[i] == '(') {
+            stack1.push(inf[i]);
+        } else if ((inf[i] >= '0') && (inf[i] <= '9')) {
+            end += inf[i];
+            end += ' ';
+        } else if (inf[i] == ')') {
+            while ((!stack1.isEmpty()) && (stack1.get() != '(')) {
+                end += stack1.pop();
+                end += ' ';
             }
-            stack.pop();
-        } else if (c == '+' || c == '-' || c == '*' || c == '/') {
-            while (!stack.isEmpty() && 
-                   (stack.top() == '*' || stack.top() == '/' || 
-                    (c == '+' || c == '-') && (stack.top() == '+' || stack.top() == '-'))) {
-                postfix += " ";
-                postfix += stack.top();
-                stack.pop();
+            if (stack1.get() == '(') {
+                stack1.pop();
             }
-            stack.push(c);
-            postfix += " ";
+        } else if (inf[i] == '+' || inf[i] == '-') {
+            if (!stack1.isEmpty()) {
+                switch (stack1.get()) {
+                    case '*': {
+                        end += '*';
+                        end += ' ';
+                        stack1.pop();
+                        break;
+                    }
+                    case '/': {
+                        end += '/';
+                        end += ' ';
+                        stack1.pop();
+                        break;
+                    }
+                    case '+': {
+                        end += '+';
+                        end += ' ';
+                        stack1.pop();
+                        break;
+                    }
+                    case '-': {
+                        end += '-';
+                        end += ' ';
+                        stack1.pop();
+                        break;
+                    }
+                }
+                stack1.push(inf[i]);
+            } else {
+                stack1.push(inf[i]);
+            }
+        } else if (inf[i] == '*' || inf[i] == '/') {
+            if (!stack1.isEmpty()) {
+                switch (stack1.get()) {
+                    case '*': {
+                        end += '*';
+                        end += ' ';
+                        stack1.pop();
+                        break;
+                    }
+                    case '/': {
+                        end += '/';
+                        end += ' ';
+                        stack1.pop();
+                        break;
+                    }
+                }
+                stack1.push(inf[i]);
+            } else {
+                stack1.push(inf[i]);
+            }
         }
     }
-
-    while (!stack.isEmpty()) {
-        postfix += " ";
-        postfix += stack.top();
-        stack.pop();
+    if (!stack1.isEmpty()) {
+        while (!stack1.isEmpty()) {
+            end += stack1.pop();
+            end += ' ';
+        }
     }
-
-    return postfix;
+    std::string end2;
+    for (int i = 0; i < (end.length() - 1); i++) {
+        end2 += end[i];
+    }
+    return end2;
   return std::string("");
 }
 
 int eval(std::string pref) {
   // добавьте код
-  TStack<int, 100> stack;
-    std::stringstream ss(post);
-    std::string token;
-    while (ss >> token) {
-        if (std::isdigit(token[0])) {
-            stack.push(std::stoi(token));
-        } else if (token[0] == '+' || token[0] == '-' || token[0] == '*' || token[0] == '/') {
-            int operand2 = stack.top();
-            stack.pop();
-            int operand1 = stack.top();
-            stack.pop();
-            if (token[0] == '+') {
-                stack.push(operand1 + operand2);
-            } else if (token[0] == '-') {
-                stack.push(operand1 - operand2);
-            } else if (token[0] == '*') {
-                stack.push(operand1 * operand2);
-            } else if (token[0] == '/') {
-                stack.push(operand1 / operand2);
+  TStack<int, 100> stack2;
+    for (int i = 0; i < pref.length(); i++) {
+        if ((pref[i] >= '0') && (pref[i] <= '9')) {
+            stack2.push(pref[i] - '0');
+        } else if (pref[i] != ' ' || (pref[i] < '0') && (pref[i] > '9')) {
+            int n1 = stack2.pop();
+            int n2 = stack2.pop();
+            switch (pref[i]) {
+            case '+':
+                stack2.push(n2 + n1);
+                break;
+            case '-':
+                stack2.push(n2 - n1);
+                break;
+            case '*':
+                stack2.push(n2 * n1);
+                break;
+            case '/':
+                stack2.push(n2 / n1);
+                break;
             }
         }
     }
-    return stack.top();
+    return stack2.get();
   return 0;
 }
